b78f704f2e1ebc216e8e748c080b36578d90e2fd
Yoann TERUEL <yoann.teruel@gmail.fr>
2019-10-28 17:44:22 +0100

    Ajout du Getbyid, Post, Put, Delete pour User

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,15 +4,10 @@ namespace App\Http\Controllers;
 
 
 use App\User;
+use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth:api');
@@ -20,7 +15,80 @@ class UsersController extends Controller
 
     public function getAllUsers()
     {
-        $user = User::all();
-        return response()->json($user);
+        $user = User::with('usr_roe_id', 'usr_sun_id')->get();
+        return $user;
+    }
+
+    public function getUserById(int $id)
+    {
+        $user = User::with('usr_roe_id', 'usr_sun_id')->find($id);
+        return $user;
+    }
+
+    public function postUser(Request $request)
+    {
+        // validation des champs
+        try {
+            $this->validate($request, [
+                'usr_pseudo' => 'required',
+                'usr_email' => 'required',
+                'password' => 'required',
+                'usr_firstname' => 'required',
+                'usr_lastname' => 'required',
+                'usr_roe_id' => 'required|integer',
+                'usr_sun_id' => 'required|integer'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => '3000'], 500);
+        }
+
+        $user = new User();
+        $user->usr_email = strtolower($request->usr_email);
+        $user->usr_pseudo = strtolower($request->usr_pseudo);
+        $user->password = password_hash($request->password, PASSWORD_BCRYPT);
+        $user->usr_firstname = $request->usr_firstname;
+        $user->usr_lastname = $request->usr_lastname;
+        $user->usr_roe_id = $request->usr_roe_id;
+        $user->usr_sun_id = $request->usr_sun_id;
+        $user->usr_activ = $request->usr_activ;
+        $user->save();
+        return $user;
+    }
+
+    public function putUserById(Request $request, int $id)
+    {
+        // validation des champs
+        try {
+            $this->validate($request, [
+                'usr_pseudo' => 'required',
+                'usr_email' => 'required',
+                'password' => 'required',
+                'usr_firstname' => 'required',
+                'usr_lastname' => 'required',
+                'usr_roe_id' => 'required|integer',
+                'usr_sun_id' => 'required|integer'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => '3000', 'message' => $e], 401);
+        }
+
+        $user = User::find($id);
+        $user->usr_email = strtolower($request->usr_email);
+        $user->usr_pseudo = strtolower($request->usr_pseudo);
+        $user->password = password_hash($request->password, PASSWORD_BCRYPT);
+        $user->usr_firstname = $request->usr_firstname;
+        $user->usr_lastname = $request->usr_lastname;
+        $user->usr_roe_id = $request->usr_roe_id;
+        $user->usr_sun_id = $request->usr_sun_id;
+        $user->usr_activ = $request->usr_activ;
+        $user->save();
+        return $user;
+    }
+
+    public function deleteUserById(int $id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['message' => 'Deleted !']);
     }
 }
