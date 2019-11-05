61d0681c720e490609bb6e53b2ccd7b2cc83fc79
audrey <nunes.audrey31@gmail.com>
2019-11-05 10:58:31 +0100

    [UPD] data structure

--- database/database_structure.sql
+++ database/database_structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Nov 05, 2019 at 09:04 AM
+-- Generation Time: Nov 05, 2019 at 09:56 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.10
 
