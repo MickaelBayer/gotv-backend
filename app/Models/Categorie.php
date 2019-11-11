14785e827a2e9b70723f4f2dd95390469e952c98
Yoann TERUEL <yoann.teruel@gmail.com>
2019-11-11 15:33:50 +0100

    Récupération des séries en fonction des catégories

--- app/Models/Categorie.php
+++ app/Models/Categorie.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Categories extends Model
+class Categorie extends Model
 {
     protected $primaryKey = "cae_id";
     
@@ -12,4 +12,8 @@ class Categories extends Model
         'cae_id_tmdb',
         'cae_label'
     ];
+
+    public function cae_series() {
+        return $this->belongsToMany('App\Models\Serie', "categories_series", "cae_see_category", "cae_see_serie");
+    }
 }
